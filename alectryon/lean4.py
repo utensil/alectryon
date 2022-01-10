3350fe46b26b9d3ca3992bb964303dc3316ff857
Niklas Bülow <me@niklasbuelow.com>
2022-01-10 21:41:27 +0100

    typeid: use updated LeanInk flags

--- alectryon/lean4.py
+++ alectryon/lean4.py
@@ -36,7 +36,7 @@ class Lean4(CLIDriver):
     ID = "leanInk"
     LANGUAGE = "lean4"
 
-    CLI_ARGS = ("analyze", "--experimental-type-tokens",)
+    CLI_ARGS = ("analyze", "--x-enable-type-info", "--x-enable-docStrings",)
 
     TMP_PREFIX = "leanInk_"
     LEAN_FILE_EXT = ".lean"
